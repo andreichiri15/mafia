2a8a487cc4edc21624495ac67e18a9e485ad598c
Andrei <andreichiri15@gmail.com>
2026-05-26 22:21:57 +0300

    added rule in config to allow websocket requests

--- backend/src/main/java/com/andreichiri/mafia_backend/config/SecurityConfig.java
+++ backend/src/main/java/com/andreichiri/mafia_backend/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/api/auth/**", "/ws/**").permitAll()
+                .requestMatchers("/api/auth/**", "/ws/**", "/api/ws/**").permitAll()
                 .anyRequest().authenticated()
             )
             .formLogin(form -> form.disable())
